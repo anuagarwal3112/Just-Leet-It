93bda5b2cd63d8ec7575d4c8289365f325b97ee7
Anu Agarwal <67804072+anuagarwal3112@users.noreply.github.com>
2022-09-09 14:38:09 +0530

    Time: 1574 ms (8.76%), Space: 125.1 MB (70.41%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    int numberOfWeakCharacters(vector<vector<int>>& nums) {
-        sort(nums.begin(),nums.end());
-        int size=nums.size();
-        int ans=0;
-        int a=nums[size-1][0];
-        int b=nums[size-1][1];
-        int lmax=nums[size-1][1];
-        
-        for(int i=size-2;i>=0;i--){
-            if(nums[i][0]<nums[i+1][0]) {
-                b=max(lmax,b);
-                lmax=max(b,nums[i][1]); 
+    bool static sorted(vector<int>&a,vector<int>&b)
+    {
+        if(a[0]==b[0])
+        {
+            return a[1]>b[1]; //[7,10],[7,9],[7,5]
+        }
+      return a[0]<b[0];
+    }
+    int numberOfWeakCharacters(vector<vector<int>>& properties) {
+      sort(properties.begin(),properties.end(),sorted);
+      int ans = 0,maxDefVal=INT_MIN;
+        int n = properties.size();
+        for(int i=n-1;i>=0;i--)
+        {
+            if(properties[i][1]<maxDefVal)
+            {
+                ans++;
             }
-            if(nums[i][0]<a && nums[i][1]<b)
-              ans++;
+            if(properties[i][1]>maxDefVal)
+            {
+              maxDefVal = properties[i][1];  
+            }
         }
         return ans;
     }
