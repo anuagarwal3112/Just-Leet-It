c98e62209051579c652d41751661aec5dc5a3fc0
Anu Agarwal <67804072+anuagarwal3112@users.noreply.github.com>
2022-09-14 19:58:19 +0530

    Time: 89 ms (5.23%), Space: 25 MB (58.25%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
     public :
     void reverse(vector<int>& nums, int i, int j){
+        int n = nums.size();
         int li = i; // left;
         int ri = j;
         
@@ -14,12 +15,11 @@ class Solution {
         }
     }
     void rotate(vector<int>& nums, int k) {
-        k = k % nums.size();
-        if(k < 0){ 
-            k += nums.size();
-        }
-        reverse(nums, 0, nums.size() - k - 1);
-        reverse(nums, nums.size() - k, nums.size() - 1);
-        reverse(nums, 0, nums.size() - 1);
+        int n = nums.size();
+        k = k % n;
+        if(k < 0) k += n;
+        reverse(nums, 0, n - k - 1);
+        reverse(nums, n - k, n - 1);
+        reverse(nums, 0, n - 1);
     }
 };
