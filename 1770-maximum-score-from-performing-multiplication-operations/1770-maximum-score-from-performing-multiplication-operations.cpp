ba747644ad4ce234aaf549329e8914178b6e52fa
Anu Agarwal <67804072+anuagarwal3112@users.noreply.github.com>
2022-09-16 14:09:11 +0530

    Time: 474 ms (84.78%), Space: 188.1 MB (11.08%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
     int solve(vector<int> &nums,vector<int> &m,int l,int i,vector<vector<long long int>> &dp){
-        
-        if(i==m.size()){
-            return 0;
-        }
-        if(dp[l][i]!=INT_MIN){
-            return dp[l][i];
-        }
+        if(i==m.size()) return 0;
+        if(dp[l][i]!=INT_MIN) return dp[l][i];
         
         int n=nums.size();
         int r=n-1-(i-l);
@@ -19,12 +14,7 @@ public:
         
     }
     int maximumScore(vector<int>& nums, vector<int>& m) {
-        
-        // n-1-(i-j);
-        
         vector<vector<long long int>> dp(m.size()+1,vector<long long int>(m.size()+1,INT_MIN));
-        
         return solve(nums,m,0,0,dp);
-        
     }
 };
