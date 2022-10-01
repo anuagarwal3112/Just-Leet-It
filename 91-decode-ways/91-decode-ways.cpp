8f2bc22db82e63ed2504897eca0bb1afaf81db8a
Anu Agarwal <67804072+anuagarwal3112@users.noreply.github.com>
2022-10-01 11:55:44 +0530

    Time: 3 ms (63.51%), Space: 6.1 MB (93.04%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    
-    int dp[102];
-    int decode(string& s, int idx, int n)
-    {
-        if(idx < n && s[idx] == '0') return 0;
-        if(idx >= n)
+    int solve (int i, string &s, vector<int> &dp) {
+        if (i >= s.size ()) 
             return 1;
+        if (s[i] == '0') 
+            return 0;
         
-        if(dp[idx] != -1) return dp[idx];
-        
-        int ways = 0;
-        
-        // Pick single
-        if(s[idx] != '0') ways = decode(s, idx+1, n);
-            
-        // Pick couple
-        if(idx+1 < n && ((s[idx] == '1' && s[idx+1] <= '9') || (s[idx]=='2' && s[idx+1] < '7')))
-           ways += decode(s, idx+2, n);
-           
-        return dp[idx] = ways;
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+        int take1 = solve (i+1,s,dp);
+        int take2 = 0;
+        if (i+2 <= s.size () && s[i] > '0' && s[i] <= '2') {
+            if (s[i] == '2' && s[i+1] >= '7' && s[i+1] <= '9') {
+                take2 = 0;
+            }
+            else
+                take2 = solve (i+2,s,dp);
+        }
+        return dp[i] = take1+take2;
     }
-    
     int numDecodings(string s) {
-       
-        int n = s.size();
-        memset(dp, -1, sizeof(dp));
-        return decode(s, 0, n);
+        int n = s.size ();
+        vector<int> dp (n, -1);
+        return solve (0,s,dp);
     }
 };
