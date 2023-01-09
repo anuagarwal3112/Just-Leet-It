4f11ad8a71b78be2824ec0a5bd8bec4780f5c24a
Anu Agarwal <67804072+anuagarwal3112@users.noreply.github.com>
2023-01-09 19:27:07 +0530

    Added solution - LeetHub

---
+++ Median of 2 Sorted Arrays of Different Sizes - GFG/median-of-2-sorted-arrays-of-different-sizes.cpp
@@ -0,0 +1,71 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+double MedianOfArrays(vector<int>& array1, vector<int>& array2);
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    double MedianOfArrays(vector<int>& array1, vector<int>& array2)
+    {
+        int n = array1.size();
+        int m = array2.size();
+        if(n>m) 
+            return MedianOfArrays(array2, array1);
+        
+        int l = 0, h = n, mp = (n+m+1)/2;
+        
+        while(l<=h){
+            int cut1 = l + (h-l)/2;
+            int cut2 = mp - cut1;
+            
+            int l1 = (cut1 == 0) ? INT_MIN : array1[cut1-1];
+            int l2 = (cut2 == 0) ? INT_MIN : array2[cut2-1];
+            int r1 = (cut1 == n) ? INT_MAX : array1[cut1];
+            int r2 = (cut2 == m) ? INT_MAX : array2[cut2];
+            
+            if(l1<=r2 && l2<=r1){
+                if((n+m)%2==0){
+                    return (max(l1,l2) + min(r1,r2))/2.0;
+                }
+                else return max(l1,l2);
+            }
+            else if(l1>r2) h = cut1-1;
+            else l = cut1+1;
+        }
+        return 0.0;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int m,n;
+        cin>>m;
+        vector<int> array1(m);
+        for (int i = 0; i < m; ++i){
+            cin>>array1[i];
+        }
+        cin>>n;
+        vector<int> array2(n);
+        for (int i = 0; i < n; ++i){
+            cin>>array2[i];
+        }
+        Solution ob;
+        cout<<ob.MedianOfArrays(array1, array2)<<endl;
+    }
+    return 0; 
+}
+
+
+  // } Driver Code Ends
